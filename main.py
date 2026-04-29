bcb7d431e0878ec6e44cbc53f35f39eadb291be4
SkutM <ssm011403@gmail.com>
2026-04-29 15:44:59 -0700

    adding preliminary db

--- main.py
+++ main.py
@@ -6,12 +6,20 @@ from sqlalchemy.orm import declarative_base, sessionmaker
 
 app = FastAPI()
 
-engine = create_engine("sqlite:///.test.db", echo=True)
+engine = create_engine("sqlite:///./test.db", echo=True)
 SessionLocal = sessionmaker(bind=engine)
 Base = declarative_base()
 
-users = []
-next_id = 1
+class User(Base):
+    __tablename__ = "users"
+
+    id = Column(Integer, primary_key=True)
+    name = Column(String, nullable=False)
+
+Base.metadata.create_all(bind=engine)
+
+# users = []
+# next_id = 1
 
 class EchoRequest(BaseModel):
     message: str
@@ -39,44 +47,77 @@ def echo(data: EchoRequest):
 
 @app.post("/users", response_model=UserResponse)
 def create_user(data: UserCreate):
-    global next_id
-    user = {
-        "id": next_id,
-        "name": data.name
-    }
-    users.append(user)
-    next_id += 1
+    db = SessionLocal()
+    try: 
+        user = User(name=data.name)
 
-    return user
+        db.add(user)
+        db.commit()
+        db.refresh(user)
+        return user
+    
+    finally:
+        db.close()
 
 # list[UserResponse] means "the response will be a list
 # of users, where each user matches UserResponse"
 @app.get("/users", response_model=list[UserResponse])
 def get_users():
-    return users
+    db = SessionLocal()
+    try:
+        return db.query(User).all()
+    finally:
+        db.close()
 
 @app.get("/users/{id}", response_model=UserResponse)
 def get_user_id(id: int):
-    for user in users:
-        if user["id"] == id:
-            return user
-    raise HTTPException(status_code=404, detail="User Not Found")
+    db = SessionLocal()
+    try:
+        user = db.query(User).filter(User.id == id).first()
 
-@app.delete("/users/{id}")
+        if user is None:
+            raise HTTPException(status_code=404, detail="User Not Found")
+        
+        return user
+    
+    finally:
+        db.close()
+
+@app.delete("/users/{id}", status_code=204)
 def delete_user_id(id: int):
-    for user in users:
-        if user["id"] == id:
-            users.remove(user)
-            return {"message": f"Successfully removed user {id}"}
-    raise HTTPException(status_code=404, detail="User Not Found")
+    db = SessionLocal()
+    try:
+        user = db.query(User).filter(User.id == id).first()
+
+        if user is None:
+            raise HTTPException(status_code=404, detail="User Not Found")
+        
+        db.delete(user)
+        db.commit()
+        # db.refresh(user) will fail
+
+    finally:
+        db.close()
 
 @app.put("/users/{id}", response_model=UserResponse)
 def update_user(id: int, data: UserUpdate):
-    for user in users:
-        if user["id"] == id:
-            update_data = data.model_dump(exclude_unset=True)
-            print(f"Updating user {id} with data: {update_data}")
-            for key, value in update_data.items():
-                user[key] = value
-            return user
-    raise HTTPException(status_code=404, detail="User Not Found")
+    db = SessionLocal()
+
+    try:
+        user = db.query(User).filter(User.id == id).first()
+
+        if user is None:
+                raise HTTPException(status_code=404, detail="User Not Found")
+        
+        update_data = data.model_dump(exclude_unset=True)
+
+        for key, value in update_data.items():
+            setattr(user, key, value)
+        
+        db.commit()
+        db.refresh(user)
+
+        return user
+    
+    finally:
+        db.close()
