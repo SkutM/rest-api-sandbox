9b56a2adbbc7d35c693f3fabc08bfa35c4d5d212
SkutM <ssm011403@gmail.com>
2026-05-04 15:43:33 -0700

    Update main.py

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
-from fastapi import FastAPI, HTTPException
+from fastapi import FastAPI, HTTPException, Depends
 from pydantic import BaseModel
 from typing import Optional
 from sqlalchemy import create_engine, Column, Integer, String
-from sqlalchemy.orm import declarative_base, sessionmaker
+from sqlalchemy.orm import declarative_base, sessionmaker, Session
 
 app = FastAPI()
 
@@ -17,6 +17,13 @@ class User(Base):
     name = Column(String, nullable=False)
 
 Base.metadata.create_all(bind=engine)
+
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
 
 # users = []
 # next_id = 1
@@ -45,79 +52,60 @@ def echo(data: EchoRequest):
         "result": data.message * data.times
     }
 
-@app.post("/users", response_model=UserResponse)
-def create_user(data: UserCreate):
-    db = SessionLocal()
-    try: 
-        user = User(name=data.name)
+def get_user_by_id(db: Session, id: int):
+    return db.query(User).filter(User.id == id).first()
 
-        db.add(user)
-        db.commit()
-        db.refresh(user)
-        return user
-    
-    finally:
-        db.close()
+def get_all_users(db: Session):
+    return db.query(User).all()
+
+@app.post("/users", response_model=UserResponse)
+def create_user(data: UserCreate, db: Session = Depends(get_db)):
+    user = User(name=data.name)
+
+    db.add(user)
+    db.commit()
+    db.refresh(user)
+    return user
 
 # list[UserResponse] means "the response will be a list
 # of users, where each user matches UserResponse"
 @app.get("/users", response_model=list[UserResponse])
-def get_users():
-    db = SessionLocal()
-    try:
-        return db.query(User).all()
-    finally:
-        db.close()
+def get_users(db: Session = Depends(get_db)):
+    return get_all_users(db)
 
 @app.get("/users/{id}", response_model=UserResponse)
-def get_user_id(id: int):
-    db = SessionLocal()
-    try:
-        user = db.query(User).filter(User.id == id).first()
-
-        if user is None:
-            raise HTTPException(status_code=404, detail="User Not Found")
-        
-        return user
+def get_user_id(id: int, db: Session = Depends(get_db)):
+    user = get_user_by_id(db, id)
     
-    finally:
-        db.close()
+    if user is None:
+        raise HTTPException(status_code=404, detail="User Not Found")
+    
+    return user
 
 @app.delete("/users/{id}", status_code=204)
-def delete_user_id(id: int):
-    db = SessionLocal()
-    try:
-        user = db.query(User).filter(User.id == id).first()
+def delete_user_id(id: int, db: Session = Depends(get_db)):
+    user = get_user_by_id(db, id)
 
-        if user is None:
-            raise HTTPException(status_code=404, detail="User Not Found")
-        
-        db.delete(user)
-        db.commit()
-        # db.refresh(user) will fail
-
-    finally:
-        db.close()
+    if user is None:
+        raise HTTPException(status_code=404, detail="User Not Found")
+    
+    db.delete(user)
+    db.commit()
+    # db.refresh(user) will fail
 
 @app.put("/users/{id}", response_model=UserResponse)
-def update_user(id: int, data: UserUpdate):
-    db = SessionLocal()
+def update_user(id: int, data: UserUpdate, db: Session = Depends(get_db)):
+    user = get_user_by_id(db, id)
 
-    try:
-        user = db.query(User).filter(User.id == id).first()
-
-        if user is None:
-                raise HTTPException(status_code=404, detail="User Not Found")
-        
-        update_data = data.model_dump(exclude_unset=True)
-
-        for key, value in update_data.items():
-            setattr(user, key, value)
-        
-        db.commit()
-        db.refresh(user)
-
-        return user
+    if user is None:
+        raise HTTPException(status_code=404, detail="User Not Found")
     
-    finally:
-        db.close()
+    update_data = data.model_dump(exclude_unset=True)
+
+    for key, value in update_data.items():
+        setattr(user, key, value)
+    
+    db.commit()
+    db.refresh(user)
+
+    return user
